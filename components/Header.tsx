1d9ea42020b7d30326d91275ff34102a7e6c9e2c
BuildTools <bear0369@naver.com>
2022-02-11 23:31:46 +0900

    feat: Update function for setting authMode

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,16 @@
+// AuthModal : authMode의 값에 따라 회원가입 모달과 로그인 모달을 띄우게 해주는 역할
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import { useDispatch } from "react-redux";
 import palette from "../styles/palette";
 import NextbnbLogoIcon from "../public/static/svg/logo/logo.svg";
 import NextbnbLogoTextIcon from "../public/static/svg/logo/logo_text.svg";
-import SignUpModal from "./auth/SignUpModal";
+import AuthModal from "./auth/AuthModal";
 import useModal from "../hooks/useModal";
 import { useSelector } from "../store";
 import HamburgerIcon from "../public/static/svg/header/hambrger.svg";
+import { authActions } from "../store/auth";
 
 const Container = styled.div`
   position: sticky;
@@ -84,6 +87,8 @@ const Header: React.FC = () => {
   //* 모달을 열고 닫을 boolean 값
   const { openModal, ModalPortal, closeModal } = useModal();
   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   return (
     <Container>
       <Link href="/">
@@ -97,11 +102,21 @@ const Header: React.FC = () => {
           <button
             type="button"
             className="header-sign-up-button"
-            onClick={openModal}
+            onClick={() => {
+              dispatch(authActions.setAuthMode("signup"));
+              openModal();
+            }}
           >
             회원가입
           </button>
-          <button type="button" className="header-login-button">
+          <button
+            type="button"
+            className="header-login-button"
+            onClick={() => {
+              dispatch(authActions.setAuthMode("login"));
+              openModal();
+            }}
+          >
             로그인
           </button>
         </div>
@@ -117,7 +132,7 @@ const Header: React.FC = () => {
         </button>
       )}
       <ModalPortal>
-        <SignUpModal closeModal={closeModal} />
+        <AuthModal closeModal={closeModal} />
       </ModalPortal>
     </Container>
   );
